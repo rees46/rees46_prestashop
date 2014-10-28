183e6d19233aeae1740586d1d0bc14dbc3fd7dfd
Sergey Servilovskiy <freddy.serg@gmail.com>
2014-10-28 12:54:39 +0300

    fix for image in recommenders block

--- controllers/front/recommenders.php
+++ controllers/front/recommenders.php
@@ -15,26 +15,24 @@ class Rees46RecommendersModuleFrontController extends ModuleFrontController
       $product_link = $link->getProductLink($product);
       $query = parse_url($product_link, PHP_URL_QUERY);
 
-      if( $query ) {
-          $recommend_product_link = $product_link.'&recommended_by=';
+      if ($query) {
+        $recommend_product_link = $product_link.'&recommended_by=';
       }
       else {
-          $recommend_product_link = $product_link.'?recommended_by=';
+        $recommend_product_link = $product_link.'?recommended_by=';
       }
       $cover = Product::getCover($product->id);
       $img_link = $link->getImageLink($product->link_rewrite, $cover['id_image'], 'home_default');
-      $parsed_link = parse_url($img_link);
-      if (array_key_exists('query', $parsed_link))
-        $img_path = $parsed_link['path'] + '?' + $parsed_link['query'];
+      if (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS'] == "on")
+        $img_link = 'https://'.$img_link;
       else
-        $img_path = $parsed_link['path'];
-
+        $img_link = 'http://'.$img_link;
       $arr_name = array_values($product->name);
       $p = Array(
         'name' => $arr_name[0],
         'url' => $recommend_product_link,
         'price' => $product->getPrice(!Tax::excludeTaxeOption(), null, 2),
-        'image_url' => $img_path
+        'image_url' => $img_link
       );
       array_push($products, $p);
     }
